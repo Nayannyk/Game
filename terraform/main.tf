9b509ac38d69b868fb876d87f194f376bb413e78
Nayannyk <99406754+Nayannyk@users.noreply.github.com>
2025-10-09 11:45:22 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -259,17 +259,3 @@ resource "aws_lb_listener" "game_listener" {
   }
 }
 
-# Output
-output "load_balancer_dns" {
-  value = aws_lb.game_alb.dns_name
-  description = "The DNS name of the load balancer"
-}
-
-output "ecr_repository_url" {
-  value = aws_ecr_repository.game_repo.repository_url
-}
-
-output "service_url" {
-  description = "Full URL to access the game"
-  value       = "http://${aws_lb.game_alb.dns_name}/game.html"
-}
