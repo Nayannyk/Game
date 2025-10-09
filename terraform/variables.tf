a94db958c9ad8b563afff7eeb6d3c33aa2977368
Nayannyk <99406754+Nayannyk@users.noreply.github.com>
2025-10-09 11:38:29 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,8 +15,8 @@ variable "ecr_image_tag" {
   type        = string
   default     = "latest"
 }
+
 variable "aws_account_id" {
-  description = "Your AWS Account ID"
+  description = "AWS Account ID"
   type        = string
-  default     = "127898337602"  # MANUAL ENTRY REQUIRED - Replace with your AWS Account ID
 }
