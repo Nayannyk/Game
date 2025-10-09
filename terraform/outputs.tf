eeb2b55cbe6a09c0d4b46bf58eee4e6e1d293adc
Nayannyk <99406754+Nayannyk@users.noreply.github.com>
2025-10-09 11:44:12 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,13 @@
 output "load_balancer_dns" {
-  description = "DNS name of the Application Load Balancer"
-  value       = aws_lb.game_alb.dns_name
+  value = aws_lb.game_alb.dns_name
+  description = "The DNS name of the load balancer"
+}
+
+output "ecr_repository_url" {
+  value = aws_ecr_repository.game_repo.repository_url
 }
 
 output "service_url" {
   description = "Full URL to access the game"
   value       = "http://${aws_lb.game_alb.dns_name}/game.html"
-}
-
-output "ecs_cluster_name" {
-  description = "ECS Cluster name"
-  value       = aws_ecs_cluster.game_cluster.name
-}
-
-output "ecs_service_name" {
-  description = "ECS Service name"
-  value       = aws_ecs_service.game_service.name
 }
